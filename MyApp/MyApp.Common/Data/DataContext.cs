bdb672a040acbd33540000c463105733b7eeac78
Manideep Innamuri <deepu.innamuri@gmail.com>
2022-08-10 08:46:41 +0530

    #initial Application Create

---
+++ MyApp/MyApp.Common/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Common.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+                
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
